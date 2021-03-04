8ff4853b47dfcb927d3eebfa58fb9f2306e6ea3a
davidemacchi <33602030+Davidemacchipoli@users.noreply.github.com>
2021-03-04 17:14:16 +0100

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,38 @@
+jQuery(document).ready(function() {
+  var mouseX = 0, mouseY = 0;
+  var xp = 0, yp = 0;
+
+  $(document).mousemove(function(e){
+    mouseX = e.pageX - 20;
+    mouseY = e.pageY - 20;
+  });
+
+  setInterval(function(){
+    xp += ((mouseX - xp)/6);
+    yp += ((mouseY - yp)/6);
+    $("#circle").css({left: xp +'px', top: yp +'px'});
+  }, 20);
+
+  $('#myCarousel').hide();
+});
+
+
+  $('#myworks').hide();
+
+
+setTimeout(function(){
+  $('#primascritta').fadeOut("slow");
+  $('#secondascritta').fadeOut("slow");
+}, 15000)
+
+setTimeout(function(){
+  $('#circle').removeClass("circle");
+  $('#circle').addClass("circlepost");
+}, 8000)
+
+setTimeout(function(){
+  $('#myworks').addClass("showme");
+  $('#myCarousel').fadeIn(6000);
+  $('#contacts').addClass("showme");
+    $('#about').addClass("showme");
+}, 15000)
